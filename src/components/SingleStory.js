699cfe220fbd0b59b7a51703d23927d9cfed531a
Luke Multanen <lmultanen.uchicago@gmail.com>
2022-08-24 17:24:40 -0400

    cleaned up some comments

--- src/components/SingleStory.js
+++ src/components/SingleStory.js
@@ -3,9 +3,6 @@ import { useParams } from "react-router-dom";
 import { useDispatch,useSelector } from "react-redux";
 import { fetchSingleStory } from "../features/stories/singleStorySlice";
 import CommentsList from "./CommentsList";
-// import { fetchSingleStoryAsync } from "../features/stories/singleStorySlice";
-// import { selectSingleStory } from "../features/stories/singleStorySlice";
-
 
 const SingleStory = () => {
     const params = useParams()
@@ -23,24 +20,6 @@ const SingleStory = () => {
                 return(<p key={idx}>{paragraph}</p>)
             }) : <p>{story.content}</p>}
             <h3>Responses:</h3>
-            {/* <div id='comments'>
-                {story.comments && story.comments.length ?
-                    story.comments.map((comment,idx) => {
-                        return(
-                            <div className='comment row' key={idx}>
-                                <img src={comment.author.imageUrl} />
-                                <div className='column'>
-                                    <a>
-                                    <h5>{comment.author.name}</h5>
-                                    </a>
-                                    <div>{comment.content}</div>
-                                </div>
-                            </div>
-                        )
-                    })
-                    : <></>
-                }
-            </div> */}
             <CommentsList/>
         </div>
     )
